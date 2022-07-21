5d771a4f1e22f379ad7dc6a7ca9139eaafdb77e0
Kyi Sin <kyisinshoon5101998@gmail.com>
2022-07-21 10:53:19 +0630

    javaDay1

---
+++ Day1.java
@@ -0,0 +1,22 @@
+
+public class Day1 {
+
+static int m = 100; // static variable
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n = 50; // instance variable
+		int m = 20; // primitive data type integer
+		int a[]=new int[5];  //non-primitive data type array declaration
+		System.out.println(n + m);  // arithmetic operator add
+		n++;  //unary operator postfix
+
+	}
+	void method() {
+		int data = 10; // local variable
+		double num = 1.2; // double data type
+		Boolean one = false; // boolean data type
+	}
+
+}
